e0f0a68b7fc252e82ce9ac3eca89c961d3baac64
ATSISTEMAS\orubio <orubio@atsistemas.com>
2022-09-01 12:58:05 +0200

    Delete Site

--- prueba-at3/src/components/listSites/cardItem.js
+++ prueba-at3/src/components/listSites/cardItem.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
+import { useDeleteSiteMutation } from "api/apiSlice";
 
 const CardItem = React.memo(({ data, onEdit }) => {
-  console.log(data);
+  const [viewDetails, setViewDetails] = useState(false);
+  const [deleteSite] = useDeleteSiteMutation();
+
+  const onDelete = async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    const r = await deleteSite(data._id);
+    console.log(r);
+  };
+
   return (
     <div className="card_item">
       <div className="card_inner">
@@ -12,11 +21,26 @@ const CardItem = React.memo(({ data, onEdit }) => {
           <FormattedMessage id="nickname" defaultMessage="Public Path" />
           {data.name}
         </div>
-        <div className="overlay">
-          <button type="submit" className="buy-btn" onClick={() => onEdit()}>
-            View Details
-          </button>
+        <div>
+          <span onClick={() => setViewDetails(!viewDetails)}>View Details</span>
         </div>
+        {viewDetails && (
+          <div>
+            <div className="real_name">{data.name}</div>
+            <div className="role_name">
+              <FormattedMessage id="nickname" defaultMessage="Public Path" />
+              {data.name}
+            </div>
+            <div>
+              <span onClick={() => onDelete()}>
+                <i className="fas fa-trash" />
+              </span>
+              <span onClick={() => onEdit()}>
+                <i className="fas fa-edit" />
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
